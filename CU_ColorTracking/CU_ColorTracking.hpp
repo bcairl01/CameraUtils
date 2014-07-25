610f817219252a7ed49a3566dac757abf5614dda
Brian Cairl <briancairl@gmail.com>
2014-07-24 21:59:46 -0400

    First Commit

---
+++ CU_ColorTracking/CU_ColorTracking.hpp
@@ -0,0 +1,30 @@
+/// @file  	CU_ColorTracking.hpp
+/// @brief 	Simple blob tracking library 		
+///
+///			Dependencies:
+///			=============
+///			- unistd.h
+///			- boost	
+///			- OpenCV
+///
+///	@author Brian Cairl
+/// @date 	July, 2014 [rev:1.0]
+///
+#ifndef CU_ColorTracking
+#define CU_ColorTracking
+
+
+namespace CamU
+{
+
+
+
+
+
+
+
+}
+
+
+
+#endif
