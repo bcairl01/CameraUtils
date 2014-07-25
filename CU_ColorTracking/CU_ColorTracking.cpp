be4ea8ba222407970583d69316333192b309ed8d
Brian Cairl <briancairl@gmail.com>
2014-07-24 23:42:52 -0400

    Untested color traker with variable match methods Note: UNTESTED!

---
+++ CU_ColorTracking/CU_ColorTracking.cpp
@@ -0,0 +1,174 @@
+#include <CU_ColorTracking.hpp>
+
+namespace CamU
+{
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// COLOR
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	std::ostream& operator<<( std::ostream& os, const Color& color )
+	{
+		os 	<< "<ColorF32>\t" 
+			<< bgr_f32[LC_IPL_R] << '\t' 
+			<< bgr_f32[LC_IPL_G] << '\t' 
+			<< bgr_f32[LC_IPL_B] << '\t'
+			<< "</ColorF32>\t";
+
+		os 	<< "<ColorU08>\t" 
+			<< bgr_u08[LC_IPL_R] << '\t' 
+			<< bgr_u08[LC_IPL_G] << '\t' 
+			<< bgr_u08[LC_IPL_B] << '\t'
+			<< "</ColorU08>";
+
+		return os;
+	}
+
+
+
+	Color::Color()
+	{
+		bgr_u08[LC_IPL_R]=bgr_u08[LC_IPL_G]=bgr_u08[LC_IPL_B]=0UL;
+		bgr_f32[LC_IPL_R]=bgr_f32[LC_IPL_G]=bgr_f32[LC_IPL_B]=0.0f;
+	}
+
+
+
+	Color::Color( const uint8_t R08, const uint8_t G08, const uint8_t B08 )
+	{
+		bgr_u08[LC_IPL_R]=R08;
+		bgr_u08[LC_IPL_G]=G08;
+		bgr_u08[LC_IPL_B]=B08;
+
+		bgr_f32[LC_IPL_R]=((float)R08)/255.0f;
+		bgr_f32[LC_IPL_G]=((float)G08)/255.0f;
+		bgr_f32[LC_IPL_B]=((float)B08)/255.0f;
+	}
+
+
+
+	Color::Color( const float   R32, const float   G32, const float   B32 )
+	{
+		bgr_f32[LC_IPL_R]=R08;
+		bgr_f32[LC_IPL_G]=G08;
+		bgr_f32[LC_IPL_B]=B08;
+
+		bgr_u08[LC_IPL_R]=(uint8_t)(R08*255.0f);
+		bgr_u08[LC_IPL_G]=(uint8_t)(G08*255.0f);
+		bgr_u08[LC_IPL_B]=(uint8_t)(B08*255.0f);
+	}
+
+
+
+	Color::Color( Pixel* _pxl )
+	{
+		bgr_u08[LC_IPL_R]=_pxl[LC_IPL_R];
+		bgr_u08[LC_IPL_G]=_pxl[LC_IPL_G];
+		bgr_u08[LC_IPL_B]=_pxl[LC_IPL_B];
+
+		bgr_f32[LC_IPL_R]=((float)R08)/255.0f;
+		bgr_f32[LC_IPL_G]=((float)G08)/255.0f;
+		bgr_f32[LC_IPL_B]=((float)B08)/255.0f;
+	}
+
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// MATCH-METHODS
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	namespace MatchMethods
+	{
+		float linear( Pixel& _pxl, Color& target, void*& params )
+		{
+			Color cpxl(_pxl);
+			float diff = 0.0f;
+			for( size_t idx = 0; idx < 3UL; idx++ )
+				diff += fabs(cpxl.bgr_f32[idx] - target.bgr_f32[idx])/3UL;
+
+			return float;
+		}
+
+
+		float poly  ( Pixel& _pxl, Color& target, void*& params )
+		{
+			float order = 2.0f;
+
+			if(params!=NULL)
+				order = *((float*)params);
+			
+			return powf(linear(_pxl,target),order);
+		}
+
+
+
+		float fuzzy ( Pixel& _pxl, Color& target, void*& params )
+		{
+			float center 		= 0.5f;
+			float temperature	= 1.0f;
+
+			if(params!=NULL)
+			{
+				center 		= *((float*)params+0UL);
+				temperature = *((float*)params+1UL);
+			}
+
+			float match_b = linear(_pxl,target)-center;
+			return 0.5f + 0.5f/(1+expf(match_b*temperature));
+		}
+	}		
+
+
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// COLOR TRACKER
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	std::ostream& operator<<( std::ostream& os, const ColorTracker& color_tracker )
+	{
+		os 	<< "<Point>\t" 
+			<< pt.x << '\t' 
+			<< pt.y << '\t' 
+			<< "</Point>\t";
+
+		os 	<< "<Saturation>\t" 
+			<< saturation << '\t' 
+			<< "</Saturation>";
+
+		return os;
+	}
+
+
+	void ColorTracker::process( IplImage*& _ipl, MatchMethod _mfn, Color& target, void* params = NULL )
+	{
+		cv::Point2f u_pt; 
+		float 	 	u_sat;
+		float 		match;
+		float 		denom = (_ipl->height * _ipl->width);
+
+		u_pt.x = u_pt.y = 0.0f;
+
+		/// Get Mean Location with provide matching function
+		for(size_t i = 0; i < _ipl->height; i++) //height of frame pixels
+		{
+			for(size_t j = 0; j < _ipl->width; j++) //width of frame pixels
+			{
+				match = _mfn(LC_IPL_PXL(_ipl,i,j),target,params);
+
+				u_pt.x 	+= (float)j;
+				u_pt.y 	+= (float)i;
+				u_sat	+=	match;
+			}
+		}
+		u_pt.x/=denom;
+		u_pt.y/=denom;
+		u_sat /=denom;
+
+		/// Update Track-Point
+		pt.x 		+= gain*( u_pt.x - pt.x );
+		pt.y 		+= gain*( u_pt.y - pt.y );
+		saturation 	+= gain*( u_sat  - saturation );
+	}
+
+}
+
--- CU_ColorTracking/CU_ColorTracking.hpp
+++ CU_ColorTracking/CU_ColorTracking.hpp
@@ -13,18 +13,87 @@
 #ifndef CU_ColorTracking
 #define CU_ColorTracking
 
+	/// C-STD
+	#include <stdio.h>  
+	#include <stdlib.h>  
 
-namespace CamU
-{
+	/// CXX-STD
+	#include <iostream>
+	#include <cmath>
+
+	/// OpenCV
+	#include "opencv/cv.h"  
+	#include "opencv2/core/core_c.h"  
+	#include "opencv2/highgui/highgui_c.h"  
+
+
+	namespace CamU
+	{
+		class Color;
+		class ColorTracker;
+
+
+		/// IPL Macros
+		#define LC_IPL_R 							2UL
+		#define LC_IPL_G							1UL
+		#define LC_IPL_B							0UL
+		#define LC_IPL_ITR(img,i,j,c) 				img->imageData[i*(img->nChannels*img->width) + j*img->nChannels + c]
+		#define LC_IPL_PXL(img,i,j) 				(img->imageData + i*(img->nChannels*img->width) + j*img->nChannels)
+
+
+		typedef uint8_t Pixel[3UL];
+		typedef float(*MatchMethod)(Pixel&,Color&,void*&);
+
+
+		namespace MatchMethods
+		{
+		float linear( Pixel& _pxl, Color& target, void*& params );
+		float poly  ( Pixel& _pxl, Color& target, void*& params );
+		float fuzzy ( Pixel& _pxl, Color& target, void*& params );
+		}
 
 
 
+		class Color
+		{
+		friend std::ostream& operator<<( std::ostream& os, const Color& color );
+		public:
+			uint8_t			bgr_u08[3UL];
+			float 			bgr_f32[3UL];
+
+			Color();
+			Color( const uint8_t R08, const uint8_t G08, const uint8_t B08 );
+			Color( const float   R32, const float   G32, const float   B32 );
+			Color( Pixel& _pxl );
+			
+			virtual ~Color()
+			{}
+		};
 
 
+		class ColorTracker
+		{
+		friend std::ostream& operator<<( std::ostream& os, const ColorTracker& color_tracker );
+		private:
+			cv::Point2f		pt;
+			float 			saturation;
+			float 			gain;
+		public:
+			void process( IplImage*& _ipl, MatchMethod _mfn, Color& target, void* params = NULL );
+			
+			ColorTracker() :
+				image_handle()
+				saturation(0.0f),
+				gain(0.0f)
+			{
+				pt.x = pt.y = 0.0f;
+			}
+
+			~ColorTracker()
+			{}
+		};
 
 
-}
-
-
+	}
 
 #endif
